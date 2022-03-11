703f284e7a3344cb17d7f89df5e2d39d4fda446f
Seligmann <zachary.a.seligman@gmail.com>
2022-03-10 20:25:34 -0600

    endpoints modified

--- ttv_emote_profile_api/src/users/routes.rs
+++ ttv_emote_profile_api/src/users/routes.rs
@@ -1,40 +1,45 @@
+use crate::users::{User, Users};
+use crate::error_handler::CustomError;
+use actix_web::{delete, get, post, put, web, HttpResponse};
+use serde_json::json;
+
 #[get("/users")]
 async fn find_all() -> Result<HttpResponse, CustomError> {
-    let users = web::block(|| users::find_all()).await.unwrap();
+    let users = web::block(|| Users::find_all()).await.unwrap();
     Ok(HttpResponse::Ok().json(users))
 }
 
 #[get("/users/{id}")]
-async fn find(id: web::Path) -> Result<HttpResponse, CustomError> {
-    let user = users::find(id.into_inner())?;
+async fn find(id: web::Path<i32>) -> Result<HttpResponse, CustomError> {
+    let user = Users::find(id.into_inner())?;
     Ok(HttpResponse::Ok().json(user))
 }
 
 #[post("/users")]
-async fn create(user: web::Json) -> Result<HttpResponse, CustomError> {
-    let user = users::create(user.into_inner())?;
+async fn create(user: web::Json<User>) -> Result<HttpResponse, CustomError> {
+    let user = Users::create(user.into_inner())?;
     Ok(HttpResponse::Ok().json(user))
 }
 
 #[put("/users/{id}")]
 async fn update(
-    id: web::Path,
-    user: web::Json,
+    id: web::Path<i32>,
+    user: web::Json<User>,
 ) -> Result<HttpResponse, CustomError> {
-    let user = users::update(id.into_inner(), user.into_inner())?;
+    let user = Users::update(id.into_inner(), user.into_inner())?;
     Ok(HttpResponse::Ok().json(user))
 }
 
 #[delete("/users/{id}")]
-async fn delete(id: web::Path) -> Result<HttpResponse, CustomError> {
-    let deleted_user = users::delete(id.into_inner())?;
+async fn delete(id: web::Path<i32>) -> Result<HttpResponse, CustomError> {
+    let deleted_user = Users::delete(id.into_inner())?;
     Ok(HttpResponse::Ok().json(json!({ "deleted": deleted_user })))
 }
 
-pub fn init_routes(config: &mut web::ServiceConfig) {
-    config.service(find_all);
-    config.service(find);
-    config.service(create);
-    config.service(update);
-    config.service(delete);
+pub fn init_routes(comfig: &mut web::ServiceConfig) {
+    comfig.service(find_all);
+    comfig.service(find);
+    comfig.service(create);
+    comfig.service(update);
+    comfig.service(delete);
 }

5b367eaab3e11b5f0b6571608de5d060eedc5c8a
Seligmann <zachary.a.seligman@gmail.com>
2022-03-10 20:26:19 -0600

    bunch of small changes

--- ttv_emote_profile_api/src/main.rs
+++ ttv_emote_profile_api/src/main.rs
@@ -1,13 +1,26 @@
-use std::env;
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate diesel_migrations;
+
 use actix_web::{App, HttpServer};
+use dotenv::dotenv;
 use listenfd::ListenFd;
+use std::env;
+
+mod db;
+mod users;
+mod error_handler;
+mod schema;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
     db::init();
+
     let mut listenfd = ListenFd::from_env();
-    let mut server = HttpServer::new(|| App::new().configure(employees::init_routes));
+    let mut server = HttpServer::new(|| App::new().configure(users::init_routes));
+
     server = match listenfd.take_tcp_listener(0)? {
         Some(listener) => server.listen(listener)?,
         None => {
@@ -16,5 +29,6 @@ async fn main() -> std::io::Result<()> {
             server.bind(format!("{}:{}", host, port))?
         }
     };
+
     server.run().await
 }
